383886278883a53dfdc2370af287d7f3cac716b3
yunb2 <ybchae0801@naver.com>
2020-09-24 21:03:48 +0900

    reverse proxy 설정

--- src/main/java/me/yunb/miristudy/controller/PingController.java
+++ src/main/java/me/yunb/miristudy/controller/PingController.java
@@ -3,12 +3,15 @@ package me.yunb.miristudy.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class PingController {
 
     @GetMapping("/ping")
-    public String pong() {
-        return "pong";
+    public String pong() throws UnknownHostException {
+        return "pong" + InetAddress.getLocalHost().getHostName();
     }
 
 }
