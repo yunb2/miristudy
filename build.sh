acb5805b525db9ad39b58451dc91d828916f6757
yunb2 <ybchae0801@naver.com>
2020-09-29 20:56:47 +0900

    config beanstalk

---
+++ build.sh
@@ -0,0 +1,23 @@
+
+TEMP_DIR=./.temp
+WORKSPACE_DIR=$TEMP_DIR/workspace
+
+ARTIFACT_PATTERN=build/libs/*.jar
+
+echo 'build'
+./gradlew clean build -x test
+
+echo 'clean workspace'
+rm -rf $TEMP_DIR
+mkdir -p $WORKSPACE_DIR
+
+echo 'copy artifact'
+cp ./$ARTIFACT_PATTERN $WORKSPACE_DIR/app.jar
+
+echo 'copy beanstalk extension'
+cp -r ./beanstalk/. $WORKSPACE_DIR
+
+echo 'zip application archive'
+
+cd $WORKSPACE_DIR
+zip -r ./miristudy-api.zip .
